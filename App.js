9613b103e1355c048e7c1921fa93ffbf296fa4f1
Nicolás Ruiz <101640855+IamNewInThis@users.noreply.github.com>
2025-09-16 11:23:19 -0300

    RETURN SDK53

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import "./global.css"
-import 'react-native-gesture-handler';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/contexts/AuthContext';
-import i18n from './src/lib/18n';
 
 export default function App() {
   return (
